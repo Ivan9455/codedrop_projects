23a7173762b65669669f3e1f7bdd894ac6f79ba6
Ivan9455 <paderin94126@gmail.com>
2018-09-18 18:12:28 +0600

    task3

--- task2/script.js
+++ task2/script.js
@@ -2,14 +2,18 @@ window.onload = function () {
     load();
 };
 let load = function () {
-    let arr = ajax('GET', 'https://dog.ceo/api/breed/hound/list', false).message;//список парод
+    let str = ajax('GET', 'https://dog.ceo/api/breed/hound/list', false);
+    if (!isNaN(str)) {
+        return alert("Ошибка " + str);
+    }
+    let arr = str.message;
     let rand = Math.floor(Math.random() * arr.length);
     let hound_list = arr[rand];
     document.title = hound_list;
     let load_random_image = ajax('GET', 'https://dog.ceo/api/breed/hound/' + hound_list + '/images', false).message;
     let rand2 = Math.floor(Math.random() * load_random_image.length);
     document.getElementById("loading").innerHTML = "" +
-        "<img src='"+load_random_image[rand2]+"'>";
+        "<img src='" + load_random_image[rand2] + "'>";
 };
 
 
@@ -17,9 +21,8 @@ let ajax = function (method, url_api, asinc) {
     let xhr = new XMLHttpRequest();
     xhr.open(method, url_api, asinc);
     xhr.send();
-    if (xhr.status != 200) {
-        return xhr.status;
-    } else {
+    if (xhr.status == 200) {
         return JSON.parse(xhr.responseText);
     }
+    return xhr.status;
 };
--- task3/script.js
+++ task3/script.js
@@ -1,22 +1,38 @@
 window.onload = function () {
-    //load();
+    load_accordion();
+    load_event();
+
 };
-let load = function () {
-    let arr = ajax('GET', 'https://dog.ceo/api/breed/hound/list', false);
-    if(arr !=200){
-        return alert("Ошибка " + arr);
+
+
+let load_accordion = function () {
+    let str = ajax('GET', 'https://dog.ceo/api/breed/hound/list', false);
+    if (!isNaN(str)) {
+        return alert("Ошибка " + str);
     }
-    arr = arr.message;
-    let rand = Math.floor(Math.random() * arr.length);
-    let hound_list = arr[rand];
-    document.title = hound_list;
-    let load_random_image = ajax('GET', 'https://dog.ceo/api/breed/hound/' + hound_list + '/images', false).message;
-    let rand2 = Math.floor(Math.random() * load_random_image.length);
-    document.getElementById("loading").innerHTML = "" +
-        "<img src='"+load_random_image[rand2]+"'>";
+    let arr = str.message;
+    let info = "";
+    for (let i = 0; i < arr.length; i++) {
+        let load_random_image = ajax('GET', 'https://dog.ceo/api/breed/hound/' + arr[i] + '/images', false).message;
+        let rand2 = Math.floor(Math.random() * load_random_image.length);
+        info += "" +
+            "<div>" +
+            "<div class='item w'>" +
+            "<div class='title'>" + arr[i] + "</div>" +
+            "<div class='open_info'>" +
+            "<div class='w1 w2'></div>" +
+            "</div>" +
+            "</div>" +
+            "<div class='hidden w'>" +
+            "<img src='" + load_random_image[rand2] + "' class='w'>" +
+            "<div class='load_img w'></div>" +
+            "<div class='button_load_img'>Загрузить</div> " +
+            "</div>" +
+            "</div>";
+    }
+    //document.getElementsByClassName("title")[0].innerText.toLocaleLowerCase()
+    document.getElementsByClassName("accordion")[0].innerHTML = info;
 };
-
-
 let ajax = function (method, url_api, asinc) {
     let xhr = new XMLHttpRequest();
     xhr.open(method, url_api, asinc);
@@ -25,4 +41,41 @@ let ajax = function (method, url_api, asinc) {
         return JSON.parse(xhr.responseText);
     }
     return xhr.status;
+};
+let load_event = function () {
+    let div = document.getElementsByClassName("w1");
+    let button_load_img = document.getElementsByClassName("button_load_img");
+    for (let i = 0; i < div.length; i++) {
+        div[i].addEventListener('click', function () {
+            if (this.parentElement.parentElement.parentElement.getElementsByClassName("w")[1].classList[0] === "hidden" ||
+                this.parentElement.parentElement.parentElement.getElementsByClassName("w")[1].classList[1] === "hidden") {
+                this.classList.remove("w2");
+                this.parentElement.parentElement.parentElement.getElementsByClassName("hidden")[0].classList.add("open");
+                this.parentElement.parentElement.parentElement.getElementsByClassName("open")[0].classList.remove("hidden");
+            } else {
+                this.classList.add("w2");
+                this.parentElement.parentElement.parentElement.getElementsByClassName("open")[0].classList.add("hidden");
+                this.parentElement.parentElement.parentElement.getElementsByClassName("hidden")[0].classList.remove("open");
+            }
+        });
+        button_load_img[i].addEventListener('click', function () {
+            if (this.parentElement.getElementsByClassName("load_img")[0].innerHTML === "") {
+                this.parentElement.getElementsByClassName("load_img")[0].innerHTML =
+                    load_img(this.parentElement.parentElement.getElementsByClassName("title")[0].innerText.toLocaleLowerCase());
+            }
+        });
+    }
+};
+let load_img = function (dog_lineage) {
+    let load_random_image = ajax('GET', 'https://dog.ceo/api/breed/hound/' + dog_lineage + '/images', false)
+    if (!isNaN(load_random_image)) {
+        return alert("Ошибка " + load_random_image);
+    }
+    let json = load_random_image.message;
+    let info = "";
+    for (let i = 0; i < 10; i++) {
+        let rand2 = Math.floor(Math.random() * json.length);
+        info += "<img class='w' src='" + json[rand2] + "'>"
+    }
+    return info;
 };
