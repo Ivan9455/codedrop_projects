a09db61f13f7ce568c830c556597c67ff06ca990
Ivan9455 <paderin94126@gmail.com>
2018-09-20 23:06:16 +0600

    task4 с валидацией

--- task4/script2.js
+++ task4/script2.js
@@ -86,16 +86,16 @@ function Todo() {
     var load_id = 0;
     let time_add = function () {
         $(document).on('change', ".time", function () {
-            check_time(".time_error", ".time")
+            check_time(".time_error", ".time",new Date().getTime())
         })
     };
-    let check_time = function (block_error, block_time) {
+    let check_time = function (block_error, block_time,date) {
         if ($(block_time).val() === "") {
             $(block_error).html("Заполните поле");
             $(block_time).addClass("error");
             return false;
-        } else if (new Date().getTime() > new Date($(block_time).val()).getTime()) {
-            console.log(new Date().getTime() + " > " + new Date($(block_time).val()).getTime())
+        } else if (date > new Date($(block_time).val()).getTime()) {
+            //console.log(new Date().getTime() + " > " + new Date($(block_time).val()).getTime())
             $(block_error).html("Вы не можете создать событие задним числом");
             $(block_time).addClass("error");
             return false;
@@ -120,7 +120,7 @@ function Todo() {
     let add = function () {
         $(document).on('click', ".info_add", function () {
             if (check_textarea(".info_content", ".text_error") &
-                check_time(".time_error", ".time")) {
+                check_time(".time_error", ".time",new Date().getTime())) {
                 console.log(new Date($(".time").val()).getTime())
                 console.log(new Date().getTime())
                 console.log(escapeHtmlEntities($(".info_content").val()))
@@ -128,7 +128,7 @@ function Todo() {
                     new Date($(".time").val()).getTime(),
                     new Date().getTime(),
                     escapeHtmlEntities($(".info_content").val()));
-                console.log(JSON.stringify(json))
+                //console.log(JSON.stringify(json))
                 localStorage.setItem(++load_id, JSON.stringify(json));
                 $(".info_content").html("")
                 load_localstorage();
@@ -148,24 +148,38 @@ function Todo() {
             $('.update_text').val("");
             let str = JSON.parse(localStorage.getItem(
                 $(this.parentElement.getElementsByClassName("entry_text")[0])[0].dataset.key))
+            $(".time_create_up").html("Время создание записи  " + dat_format(str.time_create));
             $(".update_text").val(str.text);
+            $(".update_time_input").attr("value", dat_format_input(str.time));
             $(".update_text").attr("data-key",
                 escapeHtmlEntities($(this.parentElement.getElementsByClassName("entry_text")[0])[0].dataset.key));
         });
     };
     let save = function () {
         $(document).on('click', ".save", function () {
-            localStorage.setItem(
-                escapeHtmlEntities($('.update_text').attr("data-key")),
-                escapeHtmlEntities($('.update_text').val()));
-            $('.update_text').removeAttr("data-key");
-            $('.update_text').val("");
-            $(".update").css('display', 'none');
-            load_localstorage();
+            let str = JSON.parse(localStorage.getItem(
+                escapeHtmlEntities($('.update_text').attr("data-key"))));
+            if (check_textarea(".update_text", ".update_text_error") &
+                check_time( ".update_time_input_error",".update_time_input",new Date(str.time_create).getTime())) {
+                let json = new obj(
+                    new Date($(".update_time_input").val()).getTime(),
+                    new Date(str.time_create).getTime(),
+                    escapeHtmlEntities($('.update_text').val())
+                );
+                localStorage.setItem(
+                    escapeHtmlEntities($('.update_text').attr("data-key")),
+                    JSON.stringify(json));
+                $('.update_text').removeAttr("data-key");
+                $('.update_text').val("");
+                $(".update").css('display', 'none');
+                load_localstorage();
+            }
         });
     };
     let exit = function () {
         $(document).on('click', ".exit", function () {
+            $('.update_time_input').removeClass("error");
+            $('.update_text').removeClass("error");
             $('.update_text').removeAttr("data-key");
             $('.update_text').val("");
             $(".update").css('display', 'none');
@@ -178,7 +192,7 @@ function Todo() {
         for (let key in localStorage) {
             let m = JSON.parse(localStorage.getItem(key));//.replace(/"/g,'').replace(/\\/g,'');
             //console.log(JSON.parse(m))
-            console.log(m)
+            //console.log(m)
             if (isNaN(key)) {
                 localStorage.removeItem(key);
                 break;
@@ -189,18 +203,11 @@ function Todo() {
             if (re(m.text) === m.text) {
                 m.text = escapeHtmlEntities(m.text)
             }
-            let dat = new Date(m.time_create).getFullYear() + "-" +
-                f_Date(new Date(m.time_create).getMonth() + 1) + "-" +
-                f_Date(new Date(m.time_create).getDate());
-            let dat2 = f_Date(new Date(m.time).getDate()) + "-" +
-                f_Date(new Date(m.time).getMonth() + 1) + "-" +
-                new Date(m.time).getFullYear();
             str += "" +
                 "<div class='entry w'>" +
                 "<div class='entry_time w'>" +
-                "<div class='time_create'>Время создание записи  " + dat2 +
-                "</div>" +
-                "<input type='date' class='time_update' value='" + dat + "'>" +
+                "<div class='time_create'>Время создание записи  " + dat_format(m.time_create) + "</div >" +
+                "<div class='time_create'>Время события   " + dat_format(m.time) + "</div>" +
                 "</div>" +
                 "<div class='entry_text' data-key='" + key + "'>" + m.text + "</div>" +
                 "<div class='entry_edit_button'></div>" +
@@ -240,6 +247,16 @@ function Todo() {
         } else {
             return str;
         }
+    };
+    let dat_format_input = function (t) {
+        return new Date(t).getFullYear() + "-" +
+            f_Date(new Date(t).getMonth() + 1) + "-" +
+            f_Date(new Date(t).getDate());
+    };
+    let dat_format = function (t) {
+        return f_Date(new Date(t).getDate()) + "-" +
+            f_Date(new Date(t).getMonth() + 1) + "-" +
+            new Date(t).getFullYear();
     };
     this.go = function () {
         load_localstorage();
